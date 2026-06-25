939def6c405aa4eb6f529f606cccb0db07c7d413
sarthakJain1008 <sj349279@gmail.com>
2026-06-26 04:27:43 +0530

    chore: restructure repository to backend/ and enable CORS

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from src.utils.db import Base, engine
 from src.Tasks.models import TaskModel
 from src.Users.models import UserModel
@@ -9,8 +10,17 @@ Base.metadata.create_all(engine)
 
 app = FastAPI(title="This is a TO-DO Application")
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(task_routes)
 app.include_router(user_routes)
+
 
 
 
